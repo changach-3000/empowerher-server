f0ec9e6a31f9fdf561200824e8e842838cd27e39
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-10 17:48:59 +0300

    create tables for adding users

--- db/migrate/20230906183016_create_users.rb
+++ db/migrate/20230906183016_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.boolean :is_admin, default: false
 
       t.timestamps
     end
