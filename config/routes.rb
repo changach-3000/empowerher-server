aee0cfb1e42547004c2b72ffcdf365f122b55f9f
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-10 17:48:12 +0300

    add routes for login, logout and current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+
+  delete "/auth/logout", to:"session#logout"
+  get "/current_user", to:"users#current_user"
+  post '/auth/login', to: 'session#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
