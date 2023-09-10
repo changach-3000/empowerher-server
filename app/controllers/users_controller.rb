e7a05322d7800e603819cb3e6f3eac75155e53ad
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-10 17:46:23 +0300

    create a file to manage users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,57 @@
 class UsersController < ApplicationController
-end
+    skip_before_action :authorize
+    # before_action :set_user, only: [:show, :update, :destroy] # Add this line to ensure @user is set for these actions
+  
+    def index
+      @users = User.all 
+      render json: @users, status: :ok
+    end
+  
+
+    def show
+      render json: @user, status: :ok
+    end
+
+
+    def current_user 
+      user = User.find_by(id: session[:user_id])
+
+      if user
+        render json: user, status: :ok
+        
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+
+  end
+    
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      unless @user.update(user_params)
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @user.destroy
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username,:password)
+    end
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  end
